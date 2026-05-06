2016fb9178221675f7dc0ee269fb5f80493d165c
pfitznerl <131273742+pfitznerl@users.noreply.github.com>
2026-05-06 10:37:41 +0200

    right version

--- code/tests.R
+++ code/tests.R
@@ -17,7 +17,6 @@ registerDoMC(100)
 
 dir_out="sorties/"
 dir_data_with="data/donnee_avec_pearp"
-
 parametre="t"
 res="00"
 liste_ech=c("6","9","12","15","18","21","24","27","30","33","36","39","42","45","48","57","72","84") #without lead time 3 because we use the observation of lead time 3 in the random forests
@@ -29,10 +28,7 @@ liste_sta=dir(path = dir_data_with, pattern = paste("donnee_",parametre,"_00_48_
 
 liste_window=c(1253)
 
-liste_agreg<- c("bestConvex","BOA","EWA","MLpol","MLprod","NBM","UNIFORM","bestExpert","raw.arp","mos.arp","raw.cep","mos.cep")
-liste_agreg<- c("BOA","NBM")
-liste_agreg<- c("BOA","sleep_BOA","bestConvex")
-
+liste_agreg<- c("bestConvex","BOA","EWA","MLpol","MLprod","NBM","UNIFORM","bestExpert")#,"raw.arp","mos.arp","raw.cep","mos.cep")
 
 acf_max=c()
 # Array of dimensions window*exp*exp*ech*sta
@@ -44,21 +40,20 @@ for (iwindow in 1:length(liste_window)) {
         sta=liste_sta[ista]
         sta=strsplit(sta, "_")[[1]][7]
         sta=strsplit(sta, ".txt")[[1]][1]
-        print(sta)
+        # print(sta)
         for(iech in 1:length(liste_ech)) {
             ech=liste_ech[iech]
-            # print(ech)
+            print(ech)
             for (iagreg1 in 1:length(liste_agreg)){
                 agreg1=liste_agreg[iagreg1]
                 AGREG1=agreg1
                 if(AGREG1=="raw.arp"|AGREG1=="mos.arp"|AGREG1=="raw.cep"|AGREG1=="mos.cep"){#|AGREG1=="UNIFORM") {
                     AGREG1="BOA"
                 }
-                if(AGREG1=="sleep_BOA") agreg1="BOA"
-                data1=read.table(paste(dir_out,AGREG1,"_",sta,"_",window,"_",ech,".txt",sep=""),header=TRUE,sep=";") # .txt containing the data
+                data1=read.table(paste(dir_out,AGREG1,"_",sta,"_",window,"_",ech,".txt",sep=""),header=TRUE,sep=";")
                 icol1=which(colnames(data1)==agreg1)
                 e1=data1[,icol1]-data1$Y
-                idNA1=which(!is.na(e1))                
+                idNA1=which(!is.na(e1))
                 if(length(icol1)>0) e1=e1[idNA1] else e1=rep(NA,1253)
                 for (iagreg2 in 1:length(liste_agreg)) {
                     agreg2=liste_agreg[iagreg2]
@@ -66,24 +61,24 @@ for (iwindow in 1:length(liste_window)) {
                     if(AGREG2=="raw.arp"|AGREG2=="mos.arp"|AGREG2=="raw.cep"|AGREG2=="mos.cep"){#|AGREG2=="UNIFORM") {
                         AGREG2="BOA"
                     }
-                    if(AGREG2=="sleep_BOA") agreg2="BOA"
+                    # print(paste(agreg1," vs ",agreg2,"      ech : ",ech,"    sta :",sta))
                     data2=read.table(paste(dir_out,AGREG2,"_",sta,"_",window,"_",ech,".txt",sep=""),header=TRUE,sep=";")
                     icol2=which(colnames(data2)==agreg2)
                     e2=data2[,icol2]-data2$Y
-                    if(length(icol1)>0 & length(icol2)>0) e2=e2[idNA1] else e2=rep(NA,1253)
+                    if(length(icol1)>0 & length(icol2)>0) e2=e2[idNA1] else e2=rep(NA,1253)#on supprime les lignes correspondant aux NA de e1, même si on devrait avoir idNA1=idNA2 normalement
                     idNA2=which(!is.na(e2))
                     if(length(icol1)>0 & length(icol2)>0) e2=e2[idNA2] else e2=rep(NA,1253)
                     if(length(icol2)>0 & length(icol2)>0) e1=e1[idNA2] else e1=rep(NA,1253)
                     if(sum(e1-e2)!=0)acf_max=c(acf_max,max(acf(e1^2-e2^2,lag.max=1253)$acf[-1])) #to verify that the series are not autocorrelated
                     if (iagreg1!=iagreg2 & sum(e1-e2,na.rm=T)!=0) {
                         dm=dm.test(e1,e2,h=1,alternative="less",varestimator = "acf",power=2)$p.value # we consider the errors to be independent like in Schulz 2022
-                        data_qt=as.data.frame(rbind(cbind(e1,AGREG1),cbind(e2,AGREG1)))
+                        data_qt=as.data.frame(rbind(cbind(e1,agreg1),cbind(e2,agreg2)))
                         data_qt[,1]=as.numeric(data_qt[,1])
                         colnames(data_qt)=c("err","agreg")
                         qt=quantileTest(abs(e1),abs(e2),alternative="less",target.quantile=0.95)$p.value
                     } else if (sum(e1-e2,na.rm=T)==0) {
-                       dm=1
-                       qt=1
+                        dm=1
+                        qt=1
                     } else {
                         dm=NA
                         qt=NA
@@ -108,7 +103,7 @@ for (iwindow in 1:length(liste_window)) {
             sta_x_ech=length(p_vals)
             bh=p.adjust(p_vals, method = 'BH', n = sta_x_ech)
             bh=length(which(bh <= 0.05))/sta_x_ech
-            percentage_bh=100*bh #percentage of significative DM tests after BH
+            percentage_bh=100*bh/length(bh) #percentage of significative DM tests after BH
             BH_DM[iwindow,iagreg1,iagreg2]=percentage_bh
         }
     }
@@ -134,7 +129,5 @@ for (iwindow in 1:length(liste_window)) {
 
 nameRdata="out.Rdata"
 save.image(nameRdata)
-
-
 
 
